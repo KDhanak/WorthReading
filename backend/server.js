179c63c9402cb8ebb7a86a3096fb9328c67a763d
KDhanak <kishandhanak306@gmail.com>
2024-11-04 20:10:21 +1100

    made changes for the production.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import router from './routes/authRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
@@ -23,6 +22,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => console.error(`MongoDB connection error: ${error}`));
+
+export default app;
