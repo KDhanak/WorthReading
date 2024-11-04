fe6d01e35acbdf4d0da7c717e66ffdf51b9f0ae6
KDhanak <kishandhanak306@gmail.com>
2024-11-05 00:42:33 +1100

    changes.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,21 +2,22 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current `mode`
-  const env = loadEnv(mode, process.cwd(), '');
+	// Load environment variables based on the current `mode`
+	const env = loadEnv(mode, process.cwd(), '');
 
-  const baseURL = env.VITE_DEPLOYMENT === 'true' ? env.VITE_BACKEND_URL : 'http://localhost:5000';
+	const baseURL = env.VITE_DEPLOYMENT === 'true' ? env.VITE_BACKEND_URL : 'http://localhost:5000';
 
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: baseURL,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-  };
+	return {
+		base: '/',
+		plugins: [react()],
+		server: {
+			proxy: {
+				'/api': {
+					target: baseURL,
+					changeOrigin: true,
+					secure: false,
+				},
+			},
+		},
+	};
 });
