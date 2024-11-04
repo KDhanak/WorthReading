ee6535dd6296805a45a17de62f442f461d5854c2
KDhanak <kishandhanak306@gmail.com>
2024-11-04 22:16:49 +1100

    changes.

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,30 +1,3 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import router from '../routes/authRoutes'
-
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(express.json());
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello, JavaScript with Express using ES Modules!');
-});
-
-app.use('/api/auth', router);
-
-// Connect to MongoDB and start server
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((error) => console.error(`MongoDB connection error: ${error}`));
+import app from "../server/app";
 
 export default app;
