ef393fc4173f382327988d4456e12e1a7205b9f3
KDhanak <kishandhanak306@gmail.com>
2024-11-04 21:31:23 +1100

    changes.

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import router from './routes/authRoutes.js';
+import router from '../routes/authRoutes.js';
 
 dotenv.config();
 
