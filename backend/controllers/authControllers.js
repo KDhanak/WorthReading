c547642e05887da92228e498a9c5fab0ff3bb637
KDhanak <kishandhanak306@gmail.com>
2024-11-15 16:29:44 +1100

    Cookie Issue Check.

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -121,8 +121,7 @@ export const accessToken = async (req, res) => {
 	if (!token) return res.status(401).json({ message: 'No token provided' });
 
 	jwt.verify(token, process.env.JWT_SECRET, async (error, decoded) => {
-		if (error) return res.status(403).json({ message: 'Invalid token' });
-		console.log(token);
+		if (error) return res.status(403).json({ message: 'Invalid token', accessToken: token });
 
 		try {
 			const user = await User.findById(decoded.id).select('name email');
