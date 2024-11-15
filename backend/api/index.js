1535fa77e06cc6ef927dd250953e307eb30f00f1
KDhanak <kishandhanak306@gmail.com>
2024-11-15 14:26:40 +1100

    CORS Issue

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-	origin: 'https://worth-reading-frontend.vercel.app',
+	origin: '*',
 	credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
