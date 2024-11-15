c5bee83326707b8389517a1c723f5e7be2e745bb
KDhanak <kishandhanak306@gmail.com>
2024-11-15 14:37:43 +1100

    Remove console.log.

--- frontend/src/contexts/authContext.tsx
+++ frontend/src/contexts/authContext.tsx
@@ -22,7 +22,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 		const checkLoggedInUser = async () => {
 			try {
 				const token = Cookies.get('accessToken');
-				console.log(token);
 				if (token) {
 					const response = await axios.get('/api/auth/validate', { withCredentials: true});
 					console.log(response);
