de1288159071c4501bf063dec142ac1cf123ffd6
KDhanak <kishandhanak306@gmail.com>
2024-11-16 18:21:52 +1100

    Context Error.

--- frontend/src/contexts/api.ts
+++ frontend/src/contexts/api.ts
@@ -9,11 +9,6 @@ const api = axios.create({
 	withCredentials: true,
 });
 
-api.interceptors.request.use((request) => {
-	console.log('Outgoing Request:',  request)
-	return request;
-})
-
 api.interceptors.response.use(
 	(response) => response,
 	async (error) => {
