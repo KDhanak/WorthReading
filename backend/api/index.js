4326643a48769c2cca0f9d879b4a925cd6ac0534
KDhanak <kishandhanak306@gmail.com>
2024-11-16 18:51:43 +1100

    Cookie Issue.

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,6 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const F_URL = process.env.FRONTEND_URL;
 
+app.set('trust proxy', true);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -20,6 +22,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
+
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, JavaScript with Express using ES Modules!');
