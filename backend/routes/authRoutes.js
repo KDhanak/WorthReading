5838b1717da5344147c3519023edcb4b159d4ce8
KDhanak <kishandhanak306@gmail.com>
2024-11-16 17:09:10 +1100

    requestLimiter

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { loginUser, registerUser, accessToken, logoutUser, refreshAccessToken } from '../controllers/authControllers.js';
 import rateLimit from 'express-rate-limit';
 
-const loginLimiter = rateLimit({
+const requestLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 45, // Limit each IP to 10 login attempts per window
     message: 'Too many login attempts. Please try again later.',
@@ -14,13 +14,13 @@ const authRouter = express.Router();
 authRouter.post('/register', registerUser);
 
 // Login route
-authRouter.post('/login', loginLimiter, loginUser);
+authRouter.post('/login', requestLimiter, loginUser);
 
 // token validation
 authRouter.get('/validate', accessToken);
 
 // token refresh route
-authRouter.post('/refresh-token', refreshAccessToken)
+authRouter.post('/refresh-token', requestLimiter, refreshAccessToken)
 
 // logout route
 authRouter.post('/logout', logoutUser);
