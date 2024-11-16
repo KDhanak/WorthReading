5a44fdcc2ddabcb8e1c5fff4eda27920af188591
KDhanak <kishandhanak306@gmail.com>
2024-11-16 19:16:29 +1100

    Cookie Issue.

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,14 +11,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const F_URL = process.env.FRONTEND_URL;
 
 app.set('trust proxy', true);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: F_URL,
+  origin: 'https://worth-reading.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
