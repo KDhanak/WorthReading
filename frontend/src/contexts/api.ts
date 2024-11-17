e2bd78dac317fd796fa4e217ff08c9f2d6026ad5
KDhanak <kishandhanak306@gmail.com>
2024-11-17 14:21:30 +1100

    checking refreshToken.

--- frontend/src/contexts/api.ts
+++ frontend/src/contexts/api.ts
@@ -7,49 +7,25 @@ const api = axios.create({
 	withCredentials: true,
 });
 
-api.interceptors.request.use((request) => {
-	return request;
-})
-
 api.interceptors.response.use(
 	(response) => response,
 	async (error) => {
 		const originalRequest = error.config;
 
-		if (error.response?.status === 401 && !originalRequest._retry) {
+		if (error.response.status === 401 && !originalRequest._retry) {
 			originalRequest._retry = true;
-			console.error('Access token invalid or expired');
-		}
 
+			try {
+				console.error('Access token invalid ot expired. Attempting re-authentication.');
+				await api.post('/api/auth/refresh-token');
+				return api(originalRequest);
+			} catch (refreshError) {
+				console.error('Token refresh failed, redirecting to login', refreshError);
+			}
+
+		}
 		return Promise.reject(error);
 	}
 );
-
-// api.interceptors.response.use(
-// 	(response) => response,
-// 	async (error) => {
-// 		const originalRequest = error.config;
-
-// 		if (error.response.status === 401 && !originalRequest._retry) {
-// 			originalRequest._retry = true;
-
-// 			try {
-// 				const { data } = await api.post('/api/auth/refresh-token');
-// 				const newAccessToken = data.token;
-
-// 				Cookies.set('accessToken', newAccessToken);
-
-// 				api.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
-// 				originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-
-// 				return api(originalRequest);
-// 			} catch (refreshError) {
-// 				console.error('Token refresh failed', refreshError);
-// 			}
-
-// 		}
-// 		return Promise.reject(error);
-// 	}
-// );
 
 export default api;
