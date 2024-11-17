4dd723d19cf9884ad4271506d3718838ab2a2e0a
KDhanak <kishandhanak306@gmail.com>
2024-11-17 13:15:09 +1100

    Cookie Issue.

--- frontend/src/contexts/api.ts
+++ frontend/src/contexts/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = import.meta.env.VITE_DEPLOYMENT === 'true' ? import.meta.env.VITE_BACKEND_URL : 'http://localhost:5000'
 
 const api = axios.create({
-	baseURL: baseURL,
+	baseURL: 'https://worth-reading.vercel.app/',
 	withCredentials: true,
 });
 
