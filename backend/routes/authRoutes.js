caff2f28adb30015d90298db855f654db93a7c57
KDhanak <kishandhanak306@gmail.com>
2024-11-17 14:31:30 +1100

    Limited access token requests.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -20,7 +20,7 @@ authRouter.post('/login', requestLimiter, loginUser);
 authRouter.get('/validate', accessToken);
 
 // token refresh route
-authRouter.post('/refresh-token', refreshAccessToken)
+authRouter.post('/refresh-token', requestLimiter, refreshAccessToken)
 
 // logout route
 authRouter.post('/logout', logoutUser);
