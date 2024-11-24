b3b5fe862ac429b1c61a8f4735a1e5bcdbddeac3
KDhanak <kishandhanak306@gmail.com>
2024-11-24 22:48:41 +1100

    env change.

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,13 +11,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const F_URL = process.env.FRONTEND_URL;
 
 app.set('trust proxy', true);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://worth-reading-frontend.vercel.app',
+  origin: F_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
