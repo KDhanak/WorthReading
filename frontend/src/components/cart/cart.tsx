3214a3872ef33376cf8f9337a09a0684b807c10b
KDhanak <kishandhanak306@gmail.com>
2025-02-07 00:21:11 +1100

    cart issue solved.

--- frontend/src/components/cart/cart.tsx
+++ frontend/src/components/cart/cart.tsx
@@ -16,9 +16,6 @@ const Cart: React.FC = () => {
     const { isAuthenticated } = useAuth();
     const { wishlist, fetchWishlist, addItemToWishlist } = useWishlist();
 
-    if (loading) return <Loading />;
-    if (error?.code === 404 || !cart.length) return <Empty message='Your cart is empty.' />
-
     const handleRemoveItem = async (productId: string) => {
         await removeItemFromCart(productId);
     };
@@ -44,7 +41,11 @@ const Cart: React.FC = () => {
                     setToastMessage({ success: false, message: 'Failed to update the wishlist' });
                 }
             } else {
-                setToastMessage({ success: false, message: 'There was an error adding this item to your wishlist' });
+                if (isBookInWishlist(bookId)) {
+                    setToastMessage({ success: false, message: 'This item is already in your wishlist' });
+                } else {
+                    setToastMessage({ success: false, message: 'There was an error adding this item to your wishlist' });
+                }
             }
         }
         setShowToast(true);
@@ -54,12 +55,9 @@ const Cart: React.FC = () => {
         }, 3000);
     }
 
-    const isBookInWishlist = useMemo(() => {
-        return cart.map(cartItem => ({
-            id: cartItem.productId._id,
-            inWishlist: wishlist?.some(item => item.productId._id === cartItem.productId._id) ?? false
-        }));
-    }, [cart, wishlist]);
+    const isBookInWishlist = (productId: string): boolean => {
+        return wishlist?.some(item => item.productId._id === productId) ?? false;
+    };
 
     useEffect(() => {
         if (error?.code === 401) {
@@ -79,6 +77,9 @@ const Cart: React.FC = () => {
 
     const total = parseFloat((cart.reduce((total, cartItems) => total + cartItems.quantity * cartItems.price, 0)).toFixed(2));
     const delivery = 5;
+
+    if (loading) return <Loading />;
+    if (error?.code === 404 || !cart.length) return <Empty message='Your cart is empty.' />
 
     return (
         <section className="mx-52 my-10">
@@ -106,11 +107,18 @@ const Cart: React.FC = () => {
                                             <a href="#" className="text-base font-medium text-primary_4">{cartItems.productId.title}</a>
                                             <div className="flex items-center gap-4">
                                                 <button className='flex w-fit gap-1 group group-hover:text-pink-700' onClick={() => handleAddToWishlist(cartItems.productId._id)}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-5 relative ${isAuthenticated && isBookInWishlist.some(item => item.id === cartItems.productId._id) ? 'fill-pink-700 stroke-pink-700' : 'fill-none'} group-hover:fill-pink-700 group-hover:stroke-inherit cursor-pointer`}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-5 relative ${isAuthenticated && isBookInWishlist(cartItems.productId._id) ? 'fill-pink-700 stroke-pink-700' : 'fill-none'} group-hover:fill-pink-700 group-hover:stroke-inherit cursor-pointer`}>
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                                     </svg>
-                                                    <p className={`font-medium text-sm ${isAuthenticated && isBookInWishlist.find(item => item.id === cartItems.productId._id)?.inWishlist ? 'text-pink-700' : 'text-accent-primary_4_light'} group-hover:text-pink-700 cursor-pointer`}>
-                                                        {isAuthenticated && isBookInWishlist.find(item => item.id === cartItems.productId._id)?.inWishlist ? 'In Wishlist' : 'Add to Wishlist'}
+                                                    <p
+                                                        className={`font-medium text-sm ${isAuthenticated && isBookInWishlist(cartItems.productId._id)
+                                                            ? 'text-pink-700'
+                                                            : 'text-accent-primary_4_light'
+                                                            } group-hover:text-pink-700 cursor-pointer`}
+                                                    >
+                                                        {isAuthenticated && isBookInWishlist(cartItems.productId._id)
+                                                            ? 'In Wishlist'
+                                                            : 'Add to Wishlist'}
                                                     </p>
                                                 </button>
 
